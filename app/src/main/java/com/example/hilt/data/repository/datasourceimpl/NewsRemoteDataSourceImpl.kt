5d9601e16751e077507b125499874be385d5b56e
lee989898 <wnsrud1352@gmail.com>
2022-05-16 18:46:49 +0900

    [Feat] viewmodel 및 viewmodel 팩토리 구현

--- app/src/main/java/com/example/hilt/data/repository/datasourceimpl/NewsRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/hilt/data/repository/datasourceimpl/NewsRemoteDataSourceImpl.kt
@@ -5,8 +5,8 @@ import com.example.hilt.data.model.APIResponse
 import com.example.hilt.data.repository.datasource.NewsRemoteDataSource
 import retrofit2.Response
 
-class NewsRemoteDataSourceImpl(private val newsAPIService: NewsAPIService, private val country: String, private val page: Int): NewsRemoteDataSource {
-    override suspend fun getTopHeadlines(): Response<APIResponse> {
+class NewsRemoteDataSourceImpl(private val newsAPIService: NewsAPIService): NewsRemoteDataSource {
+    override suspend fun getTopHeadlines(country: String, page: Int): Response<APIResponse> {
         return newsAPIService.getTopHeadlines(country,page)
     }
 }
