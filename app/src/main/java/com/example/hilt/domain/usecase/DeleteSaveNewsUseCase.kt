4e595a72278db7917c60b8b84a9cdecb5fc0028b
lee989898 <wnsrud1352@gmail.com>
2022-05-16 16:39:48 +0900

    [Feat] domain 유스케이스 클래스 구현

--- app/src/main/java/com/example/hilt/domain/usecase/DeleteSaveNewsUseCase.kt
+++ app/src/main/java/com/example/hilt/domain/usecase/DeleteSaveNewsUseCase.kt
@@ -1,7 +1,10 @@
 package com.example.hilt.domain.usecase
 
+import com.example.hilt.data.model.Article
 import com.example.hilt.domain.repository.NewsRepository
 
 class DeleteSaveNewsUseCase(private val newsRepository: NewsRepository) {
+
+    suspend fun execute(article: Article) = newsRepository.deleteNews(article)
 
 }
--- app/src/main/java/com/example/hilt/domain/usecase/GetNewsHeadlinesUseCase.kt
+++ app/src/main/java/com/example/hilt/domain/usecase/GetNewsHeadlinesUseCase.kt
@@ -1,7 +1,12 @@
 package com.example.hilt.domain.usecase
 
+import com.example.hilt.data.model.APIResponse
+import com.example.hilt.data.util.Resource
 import com.example.hilt.domain.repository.NewsRepository
 
-class GetNewsHeadlinesUseCase(private val news: NewsRepository) {
+class GetNewsHeadlinesUseCase(private val newsRepository: NewsRepository) {
 
+    suspend fun execute(): Resource<APIResponse>{
+        return newsRepository.getNewsHeadlines()
+    }
 }
