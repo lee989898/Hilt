31f806903fe1e80149cf41a15b057a8e1e2d3c89
lee989898 <wnsrud1352@gmail.com>
2022-05-19 20:20:30 +0900

    [FEAT] NewsRepositoryImpl 구현

---
+++ app/src/main/java/com/example/hilt/data/repository/NewsRepositoryImpl.kt
@@ -0,0 +1,44 @@
+package com.example.hilt.data.repository
+
+import com.example.hilt.data.model.APIResponse
+import com.example.hilt.data.model.Article
+import com.example.hilt.data.repository.dataSource.NewsRemoteDataSource
+import com.example.hilt.data.util.Resource
+import com.example.hilt.domain.repository.NewsRepository
+import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
+
+class NewsRepositoryImpl(
+    private val newsRemoteDataSource: NewsRemoteDataSource
+): NewsRepository {
+
+    override suspend fun getNewsHeadlines(): Resource<APIResponse> {
+        return responseToResource(newsRemoteDataSource.getTopHeadlines())
+    }
+
+    override suspend fun getSearchedNews(searchQuery: String): Resource<APIResponse> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveNews(article: Article) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteNews(article: Article) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSavedNews(): Flow<List<Article>> {
+        TODO("Not yet implemented")
+    }
+
+    private fun responseToResource(response: Response<APIResponse>): Resource<APIResponse>{
+        if(response.isSuccessful){
+            response.body()?.let { result ->
+                return Resource.Success(result)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+}
