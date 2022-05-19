618522f665f66c6c23569c6004dc6f6548873c7d
lee989898 <wnsrud1352@gmail.com>
2022-05-19 18:15:16 +0900

    [Feat] NewsRepository 인터페이스 구현

--- app/src/main/java/com/example/hilt/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/example/hilt/domain/repository/NewsRepository.kt
@@ -1,4 +1,20 @@
 package com.example.hilt.domain.repository
 
-class NewsRepository {
+import com.example.hilt.data.model.APIResponse
+import com.example.hilt.data.model.Article
+import com.example.hilt.data.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface NewsRepository {
+
+    // api
+    suspend fun getNewsHeadlines(): Resource<APIResponse>
+    suspend fun getSearchedNews(searchQuery: String): Resource<APIResponse>
+
+    // local
+    suspend fun saveNews(article: Article)
+    suspend fun deleteNews(article: Article)
+    fun getSavedNews(): Flow<List<Article>>
+
+
 }
