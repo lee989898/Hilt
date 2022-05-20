1f920e6a7a696e8259714f48545150320be8ce6f
lee989898 <wnsrud1352@gmail.com>
2022-05-20 15:26:38 +0900

    [Feat] 웹 뷰 구현

--- app/src/main/java/com/example/hilt/NewsFragment.kt
+++ app/src/main/java/com/example/hilt/NewsFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.AbsListView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hilt.data.util.Resource
@@ -40,6 +41,15 @@ class NewsFragment : Fragment() {
         fragmentNewsBinding = FragmentNewsBinding.bind(view)
         viewModel = (activity as MainActivity).viewModel
         newsAdapter = (activity as MainActivity).newsAdapter
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("selected_article", it)
+            }
+            findNavController().navigate(
+                R.id.action_newsFragment_to_infoFragment,
+                bundle
+            )
+        }
         initRecyclerView()
         viewNewsList()
     }
